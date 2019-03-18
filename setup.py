089c60262729a2ddf3a85fa1d1dda7a00f97b626
Liang <elvewyn@gmail.com>
2019-03-18 20:38:18 +0800

    Setup for package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gansdojo',
+    version='0.1',
+    description='Simple framework for training Generative Adversarial Networks.',
+    url='https://github.com:nova-zl504/gansdojo',
+    author='Oliver',
+    author_email='elvewyn@gmail.com',
+    license='MIT',
+    packages=['gansdojo'],
+    zip_safe=False)
